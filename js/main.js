812d9b66a9401ed2e3edba5a1aa34bc69f70abd6
babygirl <nana_smnm@gmail.com>
2023-09-09 20:52:11 +0900

    JavaScript

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-$(function($){
+$(function() {
     $('.bg-switcher').bgSwitcher({
-        images:['images/background1.jpg', 'images/background12.jpg', 'images/background3.jpg', 'images/background4.jpg','images/background.jpg'],
+        images: ['images/background1.jpg', 'images/background12.jpg', 'images/background3.jpg', 'images/background4.jpg', 'images/background.jpg'],
         interval: 6000,
-        loop: true,
+        loop: true
+    });
+
+    $('.slider').slick({
+        dots: true
+    });
+
+    $('nav a[href^="#"]').click(function() {
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = (href === "#") ? 'html' : href;
+        var position = $(target).offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
     });
 });
-
-$('.slider').slick({
-    dots: true,
-});
-
